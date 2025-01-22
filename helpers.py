fa45a3f0d1187a714b8ece03b60831ba79562cac
JP Hwang <me@jphwang.com>
2025-01-22 18:02:28 +0000

    bugfix

--- helpers.py
+++ helpers.py
@@ -22,8 +22,8 @@ def connect_to_weaviate() -> WeaviateClient:
     client = weaviate.connect_to_local(
         port=8080,  # For Kubernetes, use 80
         headers={
-            "X-OPENAI-API-KEY": os.environ["OPENAI_API_KEY"],
-            "X-COHERE-API-KEY": os.environ["COHERE_API_KEY"],
+            # "X-OPENAI-API-KEY": os.environ["OPENAI_API_KEY"],
+            # "X-COHERE-API-KEY": os.environ["COHERE_API_KEY"],
         },
     )
     return client
